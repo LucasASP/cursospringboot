18efede863d3bd201b3e3c04434b2197ebb7c624
Lucas Augusto <lucasasp@gmail.com>
2021-01-26 18:29:13 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/lucasasp/cursospringboot/resources/CategoriaResource.java
+++ src/main/java/com/lucasasp/cursospringboot/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.lucasasp.cursospringboot.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasasp.cursospringboot.domain.Categoria;
 import com.lucasasp.cursospringboot.services.CategoriaService;
@@ -29,6 +33,21 @@ public class CategoriaResource {
 		
 		//ResponseEntity.ok() informa que a operação ocorreu com sucesso e essa resposta vai ter como corpo o objeto que buscamos
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) { //@RequestBody faz com que o objeto Json seja convertido para java automaticamente
+		obj = service.insert(obj);
+		
+		//temos que retornar a uri do novo recurso criado
+		//fromCurrentRequest pega a uri relativa (http://localhost:8080/categorias)
+		//buildAndExpand atribui o valor do id
+		//toUri converte ele para uri
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		//created gora o codigo 201 de criação e build gera a resposta
+		return ResponseEntity.created(uri).build();
 	}
 
 }
