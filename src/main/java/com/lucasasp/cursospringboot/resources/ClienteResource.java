1ac702c052a626dd622a5f7c237a50b72c82818e
Lucas Augusto <lucasasp@gmail.com>
2021-01-27 16:57:09 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/lucasasp/cursospringboot/resources/ClienteResource.java
+++ src/main/java/com/lucasasp/cursospringboot/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.lucasasp.cursospringboot.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasasp.cursospringboot.domain.Cliente;
+import com.lucasasp.cursospringboot.dto.ClienteDTO;
 import com.lucasasp.cursospringboot.services.ClienteService;
 
 //@RestController define uma classe como um Controller REST do Spring MVC
@@ -29,6 +38,50 @@ public class ClienteResource {
 		
 		//ResponseEntity.ok() informa que a operação ocorreu com sucesso e essa resposta vai ter como corpo o objeto que buscamos
 		return ResponseEntity.ok().body(obj);
+	}
+
+	//Atualiza uma categoria
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	//Deleta uma categoria
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) { 
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	//Buscando todas as categorias
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {    
+		List<Cliente> list = service.findAll();
+		
+		//Queremos apenas que as categorias sejam listadas, para isso criamos um DTO sem a lista dos produtos
+		//Convertemos uma lista de Cliente em uma lista de ClienteDTO
+		//Para isso percorremos toda a lista de Cliente a transformando em ClienteDTO (codigo abaixo transforma uma lista em outra)
+		//Streams API (https://www.devmedia.com.br/java-streams-api-manipulando-colecoes-de-forma-eficiente/37630), recurso que oferece ao desenvolvedor a possibilidade de trabalhar com conjuntos de elementos de forma mais simples e com um número menor de linhas de código.
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	//Buscando página
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue = "ASC") String direction) {    
+		//@RequestParam tornam parametros opcionais na url (ex: /categorias/page?page=0&linesPerPage=20)
+		
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		//Page não precisa de stream
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/lucasasp/cursospringboot/services/ClienteService.java
+++ src/main/java/com/lucasasp/cursospringboot/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.lucasasp.cursospringboot.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucasasp.cursospringboot.domain.Cliente;
+import com.lucasasp.cursospringboot.dto.ClienteDTO;
 import com.lucasasp.cursospringboot.repositories.ClienteRepository;
+import com.lucasasp.cursospringboot.services.exceptions.DataIntegrityException;
 import com.lucasasp.cursospringboot.services.exceptions.ObjectNotFoundException;
 
 //@Service serve para definir uma classe como pertencente à camada de Serviço da aplicação.
@@ -27,5 +34,43 @@ public class ClienteService {
 		//ObjectNotFoundException lança uma exceção personalizada, ela foi criada na camada de serviços e a camada rest (resources) que vai receber essa exceção
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	//Paginação
+	//Page é uma classe do Spring Data que encapsula informações e operações sobre paginações
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String OrderBy, String direction) {
+		//PageRequest retorna uma pagina de dados. Objeto que vai preparar as informações para q se faça a consulta e retorne a pagina de dados
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), OrderBy);
+		//Aqui automaticamente o findAll do JPA vai considerar PageRequest como argumento, uma sobrecarga de métodos, e ira nos retornar a pagina
+		return repo.findAll(pageRequest);
+	}
+	
+	//metodo auxiliar que instancia uma Cliente a partir de um DTO
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj){
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
